cb47de85e51e18d635654d8f25917c1c4221119a
ThalesLoreto <tloreto.dev@gmail.com>
2022-08-05 23:12:20 -0300

    feat: add and setup FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,42 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { query as q } from 'faunadb';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
-      authorization: {
-        params: {
-          scope: 'read:user user:email',
-        },
-      },
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email as string),
+                ),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email: user.email } }),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email as string),
+              ),
+            ),
+          ),
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
